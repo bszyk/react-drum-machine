dc386c36277a328307a8d1357cf85bddbce2838e
Brian Siklinski <bszyk.codes@gmail.com>
2020-08-25 12:03:18 -0600

    refactored keypress and padclick methods

--- src/App.js
+++ src/App.js
@@ -16,49 +16,40 @@ function App() {
     window.addEventListener('keydown', handleKeyPress);
   });
 
+  // iterate over drum library to find
+  // matching audio from keypress
   const handleKeyPress = (e) => {
-    // Gather array of acceptable key presses
-    let keycodes = drumLibrary.map((drumPad) => drumPad.keycode);
-    // Check if keypress is within the acceptable array
-    if (keycodes.includes(e.keyCode)) {
-      // If yes, identify the key pressed as a String
-      let keypress = String.fromCharCode(e.keyCode);
-      // Grab the appropriate audio tag using the the above variable
-      let audio = document.getElementById(keypress);
-      // then play audio
-      audio.play();
-      // set a variable for the parent div
-      let drumPadDiv = audio.parentNode;
-      // display the name of the audio by accessing the id of the parent div
-      setDisplay(drumPadDiv.id.replace(/_/g, ' ').toUpperCase());
-      // initiate pad animation
-      addPadAnimation(keypress, drumPadDiv);
-    }
+    drumLibrary.forEach((drum) => {
+      if (drum.keycode === e.keyCode) {
+        let audio = document.getElementById(drum.keypress);
+        audio.play();
+        setDisplay(drum.sound.replace(/_/g, ' ').toUpperCase());
+        addPadAnimation(drum.keypress, drum.sound);
+      }
+    });
   };
 
+  // iterate over drum library to find
+  // matching audio from drum pad clicked
   const padClick = (e) => {
-    // identify the corresponding audio element's id
-    let keypress = e.target.firstChild.id;
-    // use the id variable to grab the audio tag and play audio
-    document.getElementById(keypress).play();
-    // set a variable for the target div
-    let drumPadDiv = e.target;
-    // display the name of the audio by accessing the target div's id
-    setDisplay(drumPadDiv.id.replace(/_/g, ' ').toUpperCase());
-    // initiate pad animation
-    addPadAnimation(keypress, drumPadDiv);
+    drumLibrary.forEach((drum) => {
+      if (drum.sound === e.target.id) {
+        let audio = document.getElementById(drum.keypress);
+        audio.play();
+        setDisplay(drum.sound.replace(/_/g, ' ').toUpperCase());
+        addPadAnimation(drum.keypress, drum.sound);
+      }
+    });
   };
 
-  const addPadAnimation = (keypress, drumPadDiv) => {
-    // access the associated div for circle animation
+  // add (then remove) css classes for animation effect
+  // upon keypress or drum pad click
+  const addPadAnimation = (keypress, sound) => {
+    let drumPadDiv = document.getElementById(sound);
     let circle = document.getElementById(`circle-${keypress}`);
-    // identify which color is associated with the circle div
     let colorClassNum = circle.dataset.color;
-    // add appropriate class to circle div and animate circle
     circle.classList.add(colorClassNum);
-    // also add associated color class to drum pad border
     drumPadDiv.classList.add(`drum-pad-border-${colorClassNum}`);
-    // remove classess added above to complete animation effect
     setTimeout(() => {
       circle.classList.remove(circle.dataset.color);
       drumPadDiv.classList.remove(`drum-pad-border-${colorClassNum}`);
